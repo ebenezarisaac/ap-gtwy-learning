6da5b7e8fa2dc96da6f9ad1a1dfa5a1b0dea39a8
ebenezarisaac <isaacspring14@gmail.com>
2023-04-17 16:10:08 -0500

    t

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "UP"}
+
 @app.get("/cats")
 async def root():
     return {"message": "Hello from CATS"}
